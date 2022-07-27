353ae18a4d44dd5a54385a37b640c3fa958871dd
Helio Junior <heliojr.zpt@gmail.com>
2022-07-27 14:26:11 -0300

    Trabalho de State corrigido

--- src/App.js
+++ src/App.js
@@ -2,31 +2,22 @@ import React, {Component} from "react";
 
 export default class App extends Component{
 
-    state = {
-
-        aluno:{
-            nome: "Helio",
-            idade: 34,
-            comida:"Pão com ovo"
-        }
-    }
-
   render(){
         return(
             <div>
                 
-                <h1>{this.state.aluno[0].nome}</h1>
-                <h2>{this.state.aluno[0].idade}</h2>
-                <h3>{this.state.aluno[0].comida}</h3>
+                <h1>Nome: Helio</h1>
+                <h2>34 anos</h2>
+                <h3>Comida favorita: Pão com ovo e mortadela</h3>
 
-                <ul className='lista'>
-                <li>Nego Drama</li>
+                <ul>
+                <li>Negro Drama</li>
                 <li>Smile</li>
                 </ul>
 
-                <ol className='lista1'>
-                <li>Nego Drama</li>
-                <li>Smile</li>
+                <ol>
+                <li>Jesus Chorou</li>
+                <li>Mil Vidas</li>
                 </ol>
 
             </div>
